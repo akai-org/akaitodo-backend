230e5eaca9bc3c9a20bcb3e5558f1ca8a47d772c
Aeri4a <80831299+Aeri4a@users.noreply.github.com>
2024-02-17 21:11:39 +0100

    dockerize

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   type: process.env.DB_TYPE || 'mysql',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOSTNAME || 'localhost',
   port: parseInt(process.env.DB_PORT, 10) || 3306,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
