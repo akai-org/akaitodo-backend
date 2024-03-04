de3e736ab7f136e7e74aaaac9499197363427f5e
smazik02 <s.mazik2002@gmail.com>
2024-03-04 21:08:43 +0100

    migration fix part 2

--- src/database/migrations/1708378303925-BaseUsers.ts
+++ src/database/migrations/1708378303925-BaseUsers.ts
@@ -3,10 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class BaseUsers1708378303925 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `INSERT INTO users VALUES(1, "admin", "admin@local.host", "$argon2id$v=19$m=65536,t=3,p=4$VIdKpSJabFpUj4Q5LfsCyQ$+lK9AziRRoT66i2QKRlYSdSUbpQ9onbgmvqiJAfEYWA", "admin")`,
+            `INSERT INTO users VALUES(1, "admin", "admin@local.host", "$argon2id$v=19$m=65536,t=3,p=4$VIdKpSJabFpUj4Q5LfsCyQ$+lK9AziRRoT66i2QKRlYSdSUbpQ9onbgmvqiJAfEYWA", "ADMIN")`,
         );
         await queryRunner.query(
-            `INSERT INTO users VALUES(2, "user", "user@local.host", "$argon2id$v=19$m=65536,t=3,p=4$yih+ZcO7TSpF5ETgdv2G1g$pBO2WkEOy1SC6XbCfvw9hH0DxwcD0fuSfpmvD/E+w6U", "user")`,
+            `INSERT INTO users VALUES(2, "user", "user@local.host", "$argon2id$v=19$m=65536,t=3,p=4$yih+ZcO7TSpF5ETgdv2G1g$pBO2WkEOy1SC6XbCfvw9hH0DxwcD0fuSfpmvD/E+w6U", "USER")`,
         );
     }
 
