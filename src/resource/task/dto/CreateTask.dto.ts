fc366d84efdbfbee7a3d27d32cd27790e655acda
JakubTomasz <jakub0tomaszewski@gmail.com>
2024-03-11 14:51:47 +0100

    Cealn up

--- src/resource/task/dto/CreateTask.dto.ts
+++ src/resource/task/dto/CreateTask.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class CreateTaskDTO {
     @IsString()
