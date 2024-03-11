9e9cf5b9b43f094258b96700fe900245d97a7c18
JakubTomasz <jakub0tomaszewski@gmail.com>
2024-03-11 13:04:30 +0100

    Everything works

--- src/resource/task/dto/CreateTask.dto.ts
+++ src/resource/task/dto/CreateTask.dto.ts
@@ -1,19 +1,13 @@
 import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateTaskDTO {
-    @IsNumber()
-    id?: number;
-
-    @IsNumber()
-    userId?: number;
-
     @IsString()
     @IsOptional()
     name?: string = 'No name';
 
     @IsString()
     @IsOptional()
-    description?: string = 'No sescription';
+    description?: string = 'No Description';
 
     @IsOptional()
     @IsBoolean()
