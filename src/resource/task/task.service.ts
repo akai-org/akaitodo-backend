3bdcbe1fc738b8aa497507e5aa3e1a863950ba54
JakubTomasz <jakub0tomaszewski@gmail.com>
2024-03-12 13:59:35 +0100

    Fixes

--- src/resource/task/task.service.ts
+++ src/resource/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TaskEntity } from 'src/database/entities/task.entity';
 import { Repository } from 'typeorm';
@@ -11,33 +11,53 @@ export class TaskService {
         private readonly taskRepository: Repository<TaskEntity>,
     ) {}
 
-    async editTask(taskId: number,userId: number, editTask: EditTaskDTO): Promise<ReturnTaskDTO> {
-        const task = await this.taskRepository.findOneBy({ id: taskId,userId: userId });
+    async editTask(
+        taskId: number,
+        userId: number,
+        editTask: EditTaskDTO,
+    ): Promise<ReturnTaskDTO> {
+        const task = await this.taskRepository.findOneBy({
+            id: taskId,
+            userId: userId,
+        });
         if (!task) {
             throw new NotFoundException('Task not found');
         }
-        await this.taskRepository.update({ id: taskId ,userId: userId}, { ...editTask });
-        return await this.taskRepository.findOneBy({ id: taskId,userId: userId });;
+        await this.taskRepository.update(
+            { id: taskId, userId: userId },
+            { ...editTask },
+        );
+        return await this.taskRepository.findOneBy({
+            id: taskId,
+            userId: userId,
+        });
     }
-    
 
-    async getTask(userId:number,taskId: number): Promise<ReturnTaskDTO> {
-        return await this.taskRepository.findOneBy({ userId:userId,id: taskId });
-
+    async getTask(userId: number, taskId: number): Promise<ReturnTaskDTO> {
+        return await this.taskRepository.findOneBy({
+            userId: userId,
+            id: taskId,
+        });
     }
 
     async getAllTasks(userId: number): Promise<ReturnTaskDTO[]> {
         return await this.taskRepository.find({ where: { userId: userId } });
     }
 
-    async addTask(userId: number,createTaskDTO: CreateTaskDTO): Promise<ReturnTaskDTO> {
+    async addTask(
+        userId: number,
+        createTaskDTO: CreateTaskDTO,
+    ): Promise<ReturnTaskDTO> {
         const newTask = this.taskRepository.create(createTaskDTO);
         newTask.userId = userId;
         return await this.taskRepository.save(newTask);
     }
 
-    async deleteTask(userId: number,taskId: number): Promise<boolean> {
-        const task = await this.taskRepository.findOneBy({ id: taskId, userId: userId });
+    async deleteTask(userId: number, taskId: number): Promise<boolean> {
+        const task = await this.taskRepository.findOneBy({
+            id: taskId,
+            userId: userId,
+        });
         if (!task) {
             return false;
         }
