acf6c1e5c3ab646c883030d4f4975044d6011d68
smazik02 <s.mazik2002@gmail.com>
2024-03-18 01:10:10 +0100

    Revert "Cookie variant"
    This reverts commit 416eb9997f157f652fdca7c9333c88d549309d38.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,12 +6,11 @@ import {
     HttpStatus,
     Post,
     Req,
-    Res,
     UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDTO, JwtTokenDTO, RegisterDTO } from './dto';
-import { Request, Response } from 'express';
+import { Request } from 'express';
 import { GoogleGuard } from './guard';
 import { User } from '../types';
 
@@ -33,10 +32,7 @@ export class AuthController {
     @UseGuards(GoogleGuard)
     @Get('google/callback')
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    async googleAuthCallback(
-        @Req() req: Request,
-        @Res() res: Response,
-    ): Promise<void> {
+    async googleAuthCallback(@Req() req: Request): Promise<JwtTokenDTO> {
         const user: User = req.user;
         const { username, email, password } = user;
         const registerDTO: RegisterDTO = {
@@ -44,10 +40,6 @@ export class AuthController {
             email: email,
             password: password,
         };
-        res.cookie('jwt_token', {
-            accessToken: this.authservice.registerUserByGoogle(registerDTO),
-        });
-        res.redirect('http://localhost:5173');
-        // return this.authservice.registerUserByGoogle(registerDTO);
+        return this.authservice.registerUserByGoogle(registerDTO);
     }
 }
